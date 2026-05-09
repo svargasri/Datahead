774ef50b2cb5eaeee6d7e7100793cd45a2b09467
svargasri <svargasri@unal.edu.co>
2026-05-09 12:44:31 -0500

    Menu de inicio y opciones
    se agrego el menu de inico las distintas animaciones de los botones y se creo la logica para cambiar de escena o salirse del juego

---
+++ scripts/opciones.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Proyecto/scenes/menu.tscn")
+
+
+func _on_mute_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_cargar_pressed() -> void:
+	pass # Replace with function body.
